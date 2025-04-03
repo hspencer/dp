4d8493ac2ec37f58889daf85207d7eb0dd2e7639
Herbert Spencer <hspencer@ead.cl>
2025-04-03 23:49:05 +1300

    teselador

--- assets/p5/teselador.js
+++ assets/p5/teselador.js
@@ -2,18 +2,13 @@ let teselas = [];
 let numX, numY;
 let A = 100.0;
 let lineas = false;
-let grosor = 7;
+let grosor = 1.15;
 
 function setup() {
-  let w = document.getElementById('p5').offsetWidth;
-  let divP5 = document.getElementById('p5');
+  let w = document.getElementById("p5").offsetWidth;
+  let divP5 = document.getElementById("p5");
   let cnv = createCanvas(w, w);
-  
-  cnv.style.borderRadius = "50%";
-  cnv.style.border = "2px solid #0003";
-
-  cnv.parent('p5');
-  smooth();
+  cnv.parent("p5");
   strokeCap(SQUARE);
   calcularNumeroDeTeselas();
   crearTeselas();
@@ -32,12 +27,13 @@ class Tesela {
     this.ypos = _ypos;
     this.parada = _parada;
     this.giros = int(random(4));
-    this.rot = TWO_PI/3.0 * float(this.giros);
+    this.rot = (TWO_PI / 3.0) * float(this.giros);
     this.tipo = int(random(7)) + 1;
+    this.full = true;
   }
 
   dibujar() {
-    this.hTercio = sqrt(3)*this.a/6;
+    this.hTercio = (sqrt(3) * this.a) / 6;
     let dif;
     if (this.girando) {
       dif = this.rotTarget - this.rot;
@@ -50,55 +46,58 @@ class Tesela {
     push();
     translate(this.xpos, this.ypos);
     if (!this.parada) {
-      rotate(PI/3);
+      rotate(PI / 3);
     }
     rotate(this.rot);
     fill(255); // Fondo blanco para todas las teselas
-    
-    if (this.sobre) {
-      stroke('#E51515'); // Trazo rojo en rollover
-    } 
 
     noStroke();
+
+    if (this.sobre) {
+      stroke("#E51515"); // Trazo rojo en rollover
+      fill("pink");
+    }
+
+   
     beginShape();
-    vertex(-this.a/2, this.hTercio);
-    vertex(this.a/2, this.hTercio);
-    vertex(0, -2*this.hTercio);
+    vertex(-this.a / 2, this.hTercio);
+    vertex(this.a / 2, this.hTercio);
+    vertex(0, -2 * this.hTercio);
     endShape(CLOSE);
-    
+
     if (lineas) {
       strokeWeight(0.5);
       stroke(0, 100);
       beginShape();
-      vertex(-this.a/2, this.hTercio);
-      vertex(this.a/2, this.hTercio);
-      vertex(0, -2*this.hTercio);
+      vertex(-this.a / 2, this.hTercio);
+      vertex(this.a / 2, this.hTercio);
+      vertex(0, -2 * this.hTercio);
       endShape(CLOSE);
     }
     noFill();
     stroke(0);
-    switch(this.tipo) {
-    case 1:
-      this.tipo1();
-      break;
-    case 2:
-      this.tipo2();
-      break;
-    case 3:
-      this.tipo3();
-      break;
-    case 4:
-      this.tipo4();
-      break;
-    case 5:
-      this.tipo5();
-      break;
-    case 6:
-      this.tipo6();
-      break;
-    case 7:
-      this.tipo7();
-      break;
+    switch (this.tipo) {
+      case 1:
+        this.tipo1();
+        break;
+      case 2:
+        this.tipo2();
+        break;
+      case 3:
+        this.tipo3();
+        break;
+      case 4:
+        this.tipo4();
+        break;
+      case 5:
+        this.tipo5();
+        break;
+      case 6:
+        this.tipo6();
+        break;
+      case 7:
+        this.tipo7();
+        break;
     }
     pop();
     this.calc();
@@ -106,35 +105,44 @@ class Tesela {
 
   tipo1() {
     strokeWeight(grosor);
-    arc(0, -2*this.hTercio, this.a, this.a, PI/3, TWO_PI/3);
-    line(0, this.hTercio, 0, this.hTercio*0.7);
+    arc(0, -2 * this.hTercio, this.a, this.a, PI / 3, TWO_PI / 3);
+    line(0, this.hTercio, 0, this.hTercio * 0.7);
   }
 
   tipo2() {
     strokeWeight(grosor);
     beginShape();
-    vertex(-this.a/4, -this.hTercio/2);
+    vertex(-this.a / 4, -this.hTercio / 2);
     vertex(0, 0);
-    vertex(this.a/4, -this.hTercio/2);
+    vertex(this.a / 4, -this.hTercio / 2);
     endShape();
     beginShape();
     vertex(0, this.hTercio);
-    vertex(0, this.hTercio*0.8);
-    vertex(this.a*0.15, this.hTercio*0.5);
+    vertex(0, this.hTercio * 0.8);
+    vertex(this.a * 0.15, this.hTercio * 0.5);
     endShape();
   }
 
   tipo3() {
     strokeWeight(grosor);
-    bezier(-this.a/4, -this.hTercio/2, 0, 0, 0, 0, this.a/4, -this.hTercio/2);
-    arc(this.a/4, this.hTercio, this.a/2, this.a/2, PI*3, -PI/2);
+    bezier(
+      -this.a / 4,
+      -this.hTercio / 2,
+      0,
+      0,
+      0,
+      0,
+      this.a / 4,
+      -this.hTercio / 2
+    );
+    arc(this.a / 4, this.hTercio, this.a / 2, this.a / 2, PI * 3, -PI / 2);
   }
 
   tipo4() {
     strokeWeight(grosor);
-    arc(0, -2*this.hTercio, this.a, this.a, PI/3, TWO_PI/3);
+    arc(0, -2 * this.hTercio, this.a, this.a, PI / 3, TWO_PI / 3);
     noStroke();
-    fill(this.sobre ? '#E51515' : 0);
+    fill(this.sobre ? "#E51515" : 0);
     arc(0, this.hTercio, grosor, grosor, PI, -TWO_PI);
     noFill();
   }
@@ -142,8 +150,8 @@ class Tesela {
   tipo5() {
     strokeWeight(grosor);
     strokeCap(SQUARE);
-    line(-this.a/4, -this.hTercio/2, this.a/4, -this.hTercio/2);
-    line(0, this.hTercio, this.hTercio*0.15, this.hTercio*0.7);
+    line(-this.a / 4, -this.hTercio / 2, this.a / 4, -this.hTercio / 2);
+    line(0, this.hTercio, this.hTercio * 0.15, this.hTercio * 0.7);
     strokeCap(SQUARE);
   }
 
@@ -151,34 +159,48 @@ class Tesela {
     strokeWeight(grosor);
     noFill();
     strokeCap(SQUARE);
-    arc(0, 4*this.hTercio, 2*this.a, 2*this.a, -PI*2/3, -PI/3);
+    arc(0, 4 * this.hTercio, 2 * this.a, 2 * this.a, (-PI * 2) / 3, -PI / 3);
     strokeCap(SQUARE);
-    arc(0, -2*this.hTercio, this.a, this.a, PI/3, TWO_PI/3);
+    arc(0, -2 * this.hTercio, this.a, this.a, PI / 3, TWO_PI / 3);
   }
 
   tipo7() {
     strokeWeight(grosor);
     noFill();
     strokeCap(SQUARE);
-    arc(0, 4*this.hTercio, 2*this.a, 2*this.a, -PI*2/3, -PI/3);
+    arc(0, 4 * this.hTercio, 2 * this.a, 2 * this.a, (-PI * 2) / 3, -PI / 3);
     strokeCap(SQUARE);
   }
 
   calc() {
     this.distanciaMouse = dist(this.xpos, this.ypos, mouseX, mouseY);
-    this.sobre = this.distanciaMouse < sqrt(3)*this.a/6;
+    this.sobre = this.distanciaMouse < (sqrt(3) * this.a) / 6;
+  }
+
+  show() {
+    let margin = this.a/2
+    if (
+      this.xpos > margin &&
+      this.xpos < width - margin &&
+      this.ypos > margin &&
+      this.ypos < height - margin
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
 function calcularNumeroDeTeselas() {
-  numX = int(width/(A/2))+1;
-  numY = int(height/(sqrt(3)*A/2))+1;
-  teselas = new Array(numX*numY);
+  numX = int(width / (A / 2));
+  numY = int(height / ((sqrt(3) * A) / 2));
+  teselas = new Array(numX * numY);
 }
 
 function crearTeselas() {
   for (let i = 0; i < teselas.length; i++) {
-    teselas[i] = new Tesela(A*0.95, -1000, -1000, false);
+    teselas[i] = new Tesela(A * 0.95, -1000, -1000, false);
   }
 }
 
@@ -187,21 +209,28 @@ function ubicarTeselas() {
   let filaPar = true;
   for (let y = 0; y < numY; y++) {
     let teselaDerecha = true;
-    let xOffset = (filaPar && teselaDerecha) || (!filaPar && !teselaDerecha) ? 0 : A / 2;
+    let xOffset =
+      (filaPar && teselaDerecha) || (!filaPar && !teselaDerecha) ? 0 : A / 2;
     for (let x = 0; x < numX; x++) {
-      teselas[c].xpos = (A / 2 * x) + xOffset;
-      teselas[c].ypos = (sqrt(3) * A / 2 * y) - (teselaDerecha ? 0 : sqrt(3) * A / 6);
+      teselas[c].xpos = (A / 2) * x + xOffset;
+      teselas[c].ypos =
+        ((sqrt(3) * A) / 2) * y - (teselaDerecha ? 0 : (sqrt(3) * A) / 6);
       teselas[c].parada = teselaDerecha;
       c++;
       teselaDerecha = !teselaDerecha;
     }
     filaPar = !filaPar;
   }
+  for(tesela of teselas){
+    tesela.full = tesela.show(); // deja full verdadero si no está en el borde
+  }
 }
 
 function dibujarTeselas() {
   for (let tesela of teselas) {
-    tesela.dibujar();
+    if(tesela.full){
+      tesela.dibujar();
+    }
   }
 }
 
@@ -235,14 +264,14 @@ function todasMismoTipo(tipo) {
 
 function todasTipoCualquiera() {
   for (let tesela of teselas) {
-    tesela.tipo = int(random(7))+1;
+    tesela.tipo = int(random(7)) + 1;
   }
 }
 
 function todasGiran() {
   for (let tesela of teselas) {
     if (!tesela.girando) {
-      tesela.rotTarget = tesela.rot + TWO_PI/3 * float(int(random(2))+1);
+      tesela.rotTarget = tesela.rot + (TWO_PI / 3) * float(int(random(2)) + 1);
       tesela.girando = true;
     }
   }
@@ -252,7 +281,7 @@ function mousePressed() {
   if (mouseButton === LEFT) {
     for (let tesela of teselas) {
       if (tesela.sobre && !tesela.girando) {
-        tesela.rotTarget = tesela.rot + TWO_PI/3;
+        tesela.rotTarget = tesela.rot + TWO_PI / 3;
         tesela.girando = true;
       }
     }
@@ -267,23 +296,23 @@ function mousePressed() {
 }
 
 function keyPressed() {
-  if ((key === 'a' || key === 'A') && teselas[0].a <= A) agrandarTeselas();
-  if (key === 'z' || key === 'Z') achicarTeselas();
-  if (key === 's' || key === 'S') agrandarEspacio();
-  if (key === 'x' || key === 'X') achicarEspacio();
-  if (key === 'd' || key === 'D') grosor += 0.5;
-  if (key === 'c' || key === 'C' && grosor > 1) grosor -= 0.5;
-  if (key === 'g' || key === 'G') todasGiran();
-  if (key === 'l' || key === 'L') lineas = !lineas;
-  if (key === '1') todasMismoTipo(1);
-  if (key === '2') todasMismoTipo(2);
-  if (key === '3') todasMismoTipo(3);
-  if (key === '4') todasMismoTipo(4);
-  if (key === '5') todasMismoTipo(5);
-  if (key === '6') todasMismoTipo(6);
-  if (key === '7') todasMismoTipo(7);
-  if (key === '0') todasTipoCualquiera();
-  if (key === 'r' || key === 'R') {
+  if ((key === "a" || key === "A") && teselas[0].a <= A) agrandarTeselas();
+  if (key === "z" || key === "Z") achicarTeselas();
+  if (key === "s" || key === "S") agrandarEspacio();
+  if (key === "x" || key === "X") achicarEspacio();
+  if (key === "d" || key === "D") grosor += 0.5;
+  if (key === "c" || (key === "C" && grosor > 1)) grosor -= 0.5;
+  if (key === "g" || key === "G") todasGiran();
+  if (key === "l" || key === "L") lineas = !lineas;
+  if (key === "1") todasMismoTipo(1);
+  if (key === "2") todasMismoTipo(2);
+  if (key === "3") todasMismoTipo(3);
+  if (key === "4") todasMismoTipo(4);
+  if (key === "5") todasMismoTipo(5);
+  if (key === "6") todasMismoTipo(6);
+  if (key === "7") todasMismoTipo(7);
+  if (key === "0") todasTipoCualquiera();
+  if (key === "r" || key === "R") {
     calcularNumeroDeTeselas();
     crearTeselas();
     ubicarTeselas();
